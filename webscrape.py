32f1716fc8a9015eb347c73493aaf3d5d57eb6ff
Hain <hainluud@eesti.ee>
2018-11-03 11:28:49 +0200

    Kirjutasin valmis kursuste ja id-de leidja

--- webscrape.py
+++ webscrape.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup as soup
 import requests
 import re
 
-<<<<<<< HEAD
+
 ################################################################################
 # Funktsioon, mis võtab argumendiks kursuse id ja annab välja kursuse protsendid ning kursuse nime
 def get_hinded(nimi, url, id):
@@ -64,43 +64,12 @@ with requests.Session() as c: #Funktsiooni kutsed peaksid kõik toimuma selle se
     url = "https://moodle.ut.ee/login/index.php"
     USERNAME = input("Kasutajaimi: ") ## Kasutajanimi ja parool vaja sisestada
     PASSWORD = input("Parool: ") 
-=======
-"""def KM_hinded():
-        raw_page2 = c.get("https://moodle.ut.ee/grade/report/user/index.php?id=2880")
-        raw_soup2 = soup(raw_page2.content, "html.parser")
-        konteiner = raw_soup2.findAll("td")#, {"headers":"cat_5042_121018 row_43315_121018 grade"}
-        print(konteiner)"""
 
-################################################################################
-    # Funktsioon, mis võtab argumendiks kursuse id (nt 2880) ja annab välja kursuse hinnetelehelt saadud protsendid ning kursuse nime
-    # Hetkel on meile vajalikud id-d [2880, 3403, 500]#
-
-################################################################################
-
-
-with requests.Session() as c: #Funktsiooni kutsed peaksid kõik toimuma selle sessiooni jooksul#
-    url = "https://moodle.ut.ee/login/index.php"
-    USERNAME = "" ## Kasutajanimi ja parool vaja sisestada
-    PASSWORD = "" 
->>>>>>> Stardiprogramm
-    c.get(url)
-    login_data = dict(username= USERNAME, password = PASSWORD)
-    c.post(url, data=login_data)
-    raw_page = c.get("https://moodle.ut.ee/my/")
-<<<<<<< HEAD
-    raw_soup = soup(raw_page.content, "html.parser") #Muudab puhta html-i supi objektiks 
-                                                     #ning saame supi funktsioone kasutada (nt findAll)#
-=======
-    #print(raw_page.content)
-
-    raw_soup = soup(raw_page.content, "html.parser") #Muudab puhta html-i supi objektiks ning saame supi funktsioone kasutada (nt findAll)#
-    #print(raw_soup)
->>>>>>> Stardiprogramm
 
     #################################################################################
     #Programmi lõik mis loeb millistest kursustest võtab inimene osa
     container = raw_soup.findAll("div", {"class":"box coursebox"})
-<<<<<<< HEAD
+
     kõik_vajalik = []
 
     for div in container:
@@ -125,19 +94,14 @@ with requests.Session() as c: #Funktsiooni kutsed peaksid kõik toimuma selle se
 #Matplotlibi programmilõik
 from matplot import tulpdiagram #tulpdiagram(sinu_koondhinne, kursuse_keskmine,  kursuse_nimed ):
 tulpdiagram(kõik_sinu_protsendid, kõik_kursuste_protsendid, kõik_kursused)
-=======
+
     #print(container)
 
     #KM_hinded()
     
-    # ################################################################################
-    #Programmi lõik mis loeb millistest kursustest võtab inimene osa
 
-    
-    ################################################################################
-    #Matplotlibi programmilõik
 
->>>>>>> Stardiprogramm
+
 
 
 
