1fd76a401ea835ed8e5dafeb5a02db57bddcaedc
Hain <hainluud@eesti.ee>
2018-11-06 12:28:03 +0200

    Update

--- webscrape.py
+++ webscrape.py
@@ -1,6 +1,5 @@
 # Source of knowledge  = https://youtu.be/XQgXKtPSzUI
 # https://youtu.be/eRSJSKG4mDA
-from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 import requests
 
@@ -8,13 +7,31 @@ import requests
 ################################################################################
     # Funktsioon, mis võtab argumendiks kursuse id (nt 2880) ja annab välja kursuse hinnetelehelt saadud protsendid ning kursuse nime
 def get_hinded(nimi, url, id):
+    produkt = [] # (kursuse nimi, sinu tulemus %, kursuse keskmine %)
     raw_hinded = c.get("https://moodle.ut.ee/grade/report/user/index.php?id="+id)
     hinded_soup = soup(raw_hinded.content, "html.parser")
 
-    lõpphinde_container = hinded_soup.findAll("tr")[-1].find("td", {"class":"level1 levelodd oddd1 baggt b2b itemcenter column-percentage"})
+    lõpphinde_container = hinded_soup.findAll("tr")[-1]
+    #.find("td", {"class":"level1 levelodd oddd1 baggt b2b itemcenter column-percentage"})
     
-    if nimi == "Programmeerimine (LTAT.03.001)":
-        lõpphinde_container = hinded_soup.findAll("tr")[-2]
+    if lõpphinde_container.find("td", {"class":"level1 levelodd oddd1 baggt b2b itemcenter column-percentage"}) != None:
+        sinu_protsent = lõpphinde_container.find("td", {"class":"level1 levelodd oddd1 baggt b2b itemcenter column-percentage"}).text
+        kurusse_protsent = lõpphinde_container.find("td", {"class":"level1 levelodd oddd1 baggt b2b itemcenter column-average"}).text
+
+    else: #Kui protsenti pole kirjas
+        if lõpphinde_container.find("td", {"class":"level1 levelodd oddd1 baggt b2b itemcenter column-grade"}) != None:
+            
+            try:
+                punktid = float(lõpphinde_container[0].text)  ####Veel töötan selle kallal
+                max_punktid = float(lõpphinde_container[1].text[2:])
+                kursuse_keskmine = float(lõpphinde_container[2].text)
+            except:
+                punktid, max_punktid, kursuse_keskmine = "-","-","-"
+    
+
+    else:
+        punktid, max_punktid, kursuse_keskmine = 0,0,0
+        #produkt.append((nimi, punktid, ))"""
 
     print(nimi)
     print(lõpphinde_container)
