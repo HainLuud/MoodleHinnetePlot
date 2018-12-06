8b4c96f73a79bb1339768883592a21215d31cdcb
Konstantin <konstantin-r@hotmail.com>
2018-12-06 19:20:32 +0200

    tkinter

--- matplot.py
+++ matplot.py
@@ -19,7 +19,7 @@ def tulpdiagram(sinu_koondhinne, kursuse_keskmine,  kursuse_nimed ):
     plt.bar(index - 0.05, sinu_koondhinne,0.10, label='Sinu koondhinne') # SINU keskmise väärtus vastvalt indeksile
     plt.bar(index + 0.05, kursuse_keskmine,0.10, label="Kursuse keskmine") # KURSUSE keskmise väärtus vastvalt indeksile
     plt.xticks(index, kursuse_nimed, fontsize=12) # nimi vastvalt indeksile
-    #plt.ylim([0,100]) # protsendi skaala, self explanatory
+    plt.ylim([0,100]) # protsendi skaala, self explanatory
     plt.legend(loc='best') # Näitab legendi
     plt.gca().yaxis.grid(linestyle='dotted') # Näitab Y axis abistavaid jooni statistika jälgimiseks
     plt.show()
@@ -27,7 +27,7 @@ def tulpdiagram(sinu_koondhinne, kursuse_keskmine,  kursuse_nimed ):
 
 
 #näidis väärtused
-#tulpdiagram([30,40,50,80],[50,60,50,70],["prog","arhit","sisse","mate pilt"])
+tulpdiagram([30,40,50,80],[50,60,50,70],["prog","arhit","sisse","mate pilt"])
 
 # x = kursuse nimed 
 # y väärtused
