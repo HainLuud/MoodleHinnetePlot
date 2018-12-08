44b07a5d8640e156f670d369556d6dd01994ccc7
Konstantin <konstantin-r@hotmail.com>
2018-12-08 20:07:41 +0200

    Roses are red, violets are blue, here is a program for you! #nohomo

--- gui_webscrape.py
+++ gui_webscrape.py
@@ -3,59 +3,72 @@ from tkinter import *
 from PIL import ImageTk,Image
 import time
 
-#ONCLICK käivita siia tuleb siis link ülejäänud koodile teha
+#ONCLICK käivita siia tuleb siis link ülejäänud koodile teha ,Scbefx126.
 def clicked():
     #Asenda pilt
-
     jooksuta()
-    vlabel.configure(image="graafik.png")
-
-#loob akna
-window = Tk()
-window.geometry('950x600')
-window.title("Webscrape projekt")
-
-
-# loob  alam-aknad
-frame_labels = Frame(window, borderwidth="2", relief="ridge") # alam aken vormi jaoks
-frame_labels.pack(side=LEFT,padx=50, pady=20)
-frame_image = Frame(window, borderwidth="4", relief="ridge") # Pildi jaoks
-frame_image.pack(side=LEFT)
-
-
-#Vorm
-lbl1 = Label(frame_labels, text="Kasutajanimi:")
-txt1 = Entry(frame_labels,width=30)
-lbl2 = Label(frame_labels, text="Parool")
-txt2 = Entry(frame_labels,width=30, show="*")
+    #Uuus graaaaaaaafik meeeeeeees
+    photo1 = "graafik.png"
+    photo1 = ImageTk.PhotoImage(Image.open(photo1))
+    vlabel.configure(image=photo1)
+    vlabel.image = photo1
 
 #Kutse scriptile
 def jooksuta():
     myvars = {"USERNAME":txt1.get() , "PASSWORD": txt2.get()}
     exec(open("webscrape.py", "r", encoding="UTF-8").read(),myvars)
+    
+    
 
-#Kleepimine  aknale
+#loob akna
+window = Tk()
+window.geometry('970x600')
+window.title("Webscrape projekt")
+
+
+# loob  alam-aknad
+frame_labels = Frame(window, borderwidth="0", relief="ridge") # alam aken vormi jaoks
+frame_labels.pack(side=LEFT,padx=50, pady=20)
+frame_image = Frame(window, borderwidth="4", relief="ridge") # Pildi jaoks
+frame_image.pack(side=LEFT)
+
+#loob alam-alam aken
+frame_tekst = Frame(frame_labels, borderwidth="2", relief="ridge") # alam aken vormi jaoks
+frame_tekst.pack(side=TOP)
+frame_vorm = Frame(frame_labels, borderwidth="0", relief="ridge") # alam aken vormi jaoks
+frame_vorm.pack(side=BOTTOM,pady=10)
+
+
+
+# Tutvustav tekst
+tekst = Label(frame_tekst, text="Tere tulemast! \n Programm on mõeldud selleks,\n et näidata teie hinnete seis \n graafiku kujul. \n  Selleks sisestage oma Moodle \n kasutajanimi ja parool.\n Graafiku loomine võib võtta veits aega!")
+tekst.pack(pady=20)
+
+
+#Vorm
+lbl1 = Label(frame_vorm, text="Kasutajanimi:")
+txt1 = Entry(frame_vorm,width=30)
+lbl2 = Label(frame_vorm, text="Parool")
+txt2 = Entry(frame_vorm,width=30, show="*")
 lbl1.pack()
 txt1.pack()
 lbl2.pack()
 txt2.pack()
+
+
 #Nupp
-btn = Button(frame_labels, text="Näita graafikut", command=clicked)
+btn = Button(frame_vorm, text="Näita graafikut", command=clicked)
 btn.pack()
 
+#Nähtamatu kast et lükata vorm ülespoole
+frame_kast = Frame(frame_vorm, borderwidth="0", relief="ridge") # alam aken vormi jaoks
+frame_kast.pack(side=BOTTOM,pady=100)
 
 #Graafik
 photo = "graafik1.png"
-photo1 = "graafik.png"
 photo = ImageTk.PhotoImage(Image.open(photo))
-photo1 = ImageTk.PhotoImage(Image.open(photo1))
 vlabel=Label(frame_image,image=photo)
 vlabel.pack(side=LEFT)
 
 
-
-
-
-
-#
 window.mainloop()
